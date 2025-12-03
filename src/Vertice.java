0ccd2f4c4d314d4d9fbb029762a7a42b29076eb9
Absalão <absalaoresplandes@gmail.com>
2025-12-03 17:41:56 -0300

    esqueci-da-classe-vertice

--- src/Vertice.java
+++ src/Vertice.java
@@ -1,2 +1,18 @@
 public class Vertice {
+    int id;
+    int chave;
+    Vertice pai;
+    boolean visitado;
+
+    public Vertice(int id) {
+        this.id = id;
+        this.chave = Integer.MAX_VALUE;
+        this.pai = null;
+        this.visitado = false;
+    }
+
+    @Override
+    public String toString() {
+        return "V" + id;
+    }
 }
